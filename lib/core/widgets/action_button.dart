c006192263a425d07b476d7ad4bb558d59120754
ahmedjr5050 <ahmedsabry7333@gmail.com>
2023-10-05 22:22:42 +0200

    create books action

---
+++ lib/core/widgets/action_button.dart
@@ -0,0 +1,28 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({super.key, required this.backgroundColor, required this.textColor,  this.borderRadius, required this.text});
+  final String text;
+final Color backgroundColor;
+final Color textColor;
+final BorderRadius? borderRadius;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: backgroundColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadius?? BorderRadius.circular(16)
+            )
+          ),
+        child: Text(
+          text,style: Styles.textStyle18.copyWith(color: textColor,fontFamily: 'Gilroy-Bold',fontSize: 18),
+        ),
+        onPressed: (){},
+      ),
+    );
+  }
+}
--- lib/presentation/views/home/widgets/book_details_view_body.dart
+++ lib/presentation/views/home/widgets/book_details_view_body.dart
@@ -1,9 +1,12 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/presentation/views/home/widgets/book_action.dart';
+import 'package:bookly/presentation/views/home/widgets/bookrating.dart';
 import 'package:bookly/presentation/views/home/widgets/custom_book.details_app_bar.dart';
 import 'package:bookly/presentation/views/home/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
-  const BookDetailsViewBody({super.key});
+  const BookDetailsViewBody({super.key,});
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +17,19 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDet5ailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width*.17),
+            padding: EdgeInsets.symmetric(horizontal: width*.2),
             child: const CustomBookImage(),
           ),
-
+          const SizedBox(height: 43,),
+          Text('The Jungle Book',style: Styles.textStyle30.copyWith(fontWeight:FontWeight.bold)),
+             const SizedBox(height: 6,),
+          Opacity(
+            opacity: .7,
+            child: Text('Rudyard Kipling',style: Styles.textStyle18.copyWith(fontWeight: FontWeight.w500,fontStyle: FontStyle.italic))),
+             const SizedBox(height: 18,),
+      const BookRating(mainAxisAlignment: MainAxisAlignment.center,),
+      const SizedBox(height: 37,),
+     ActionB(),
         ],
       ),
     );
